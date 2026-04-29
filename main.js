257df4b4ce6a3e80e6dc297f1182606101581d73
Tomás Moreno <morenotomas2112@gmail.com>
2026-04-29 11:40:47 -0300

    hasta segundo ejercicio

---
+++ main.js
@@ -0,0 +1,18 @@
+console.log("Guía JavaScript iniciada");
+// Ejercicio 1
+let nombre = "Tomás";
+let edad = 19;
+let vida = 100;
+let puntaje = 0;
+let activo = true;
+
+console.log(nombre, edad, vida, puntaje, activo);
+
+//Ejercicio 2
+let puntosbase = 100;
+let bonus = 50;
+let penalizacion = 20;
+let puntosfinales = puntosbase + bonus - penalizacion;
+console.log(puntosfinales);
+
+//Ejercicio 3
